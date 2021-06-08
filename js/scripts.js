a69de1b6a043a654ad7573efe024a38ca73c3dd1
K Dinesh Chitra <kdineshchitra@gmail.com>
2021-06-08 14:12:29 +0530

    added skills & tools and logos

--- js/scripts.js
+++ js/scripts.js
@@ -58,7 +58,7 @@ const text = document.querySelector(".text");
 readMoreBtn.addEventListener("click", (e) => {
   text.classList.toggle("show-more");
     if (readMoreBtn.innerText === "Read More") {
-    readMoreBtn.innerText = "Read Less";
+    readMoreBtn.innerText = "Show Less";
   } else {
     readMoreBtn.innerText = "Read More";
   }
